88e0dee90e258984dae836f0a6c97cce3d5c930a
nathidaum <nathidaum@gmail.com>
2024-10-23 14:47:05 +0200

    Display movies in a gallery

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css"
 function Header() {
 return (
         <header className="Header">
-        <h1>Welcome to Popcorn Time!</h1>
+        <h1>Welcome to Popcorn Time! 🍿</h1>
         </header>
     )
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,29 @@
 import movies from "../data/movies.json";
+import { useState } from 'react';
 
 function MovieList() {
+
+  const [moviesToDisplay, setMoviesToDisplay] = useState(movies);
+
+  const deleteMovie = (movieId) => {
+    const updatedMovies = moviesToDisplay.filter((movieObj) => movieObj.id !== movieId);
+    setMoviesToDisplay(updatedMovies);
+    setNumberOfMovues(updatedMovies.length);
+  };
+
   return (
     <>
-      {movies.map((movieObj) => {
+      <h2>We currently have {moviesToDisplay.length} movies available</h2>
+      {moviesToDisplay.map((movieObj) => {
         return (
-            <div key={movieObj.id} className="card">
-                <img src={movieObj.imgURL}/>
-                <h3>{movieObj.title}</h3>
-                <p>Rating: {movieObj.rating}</p>
-            </div>
+          <div key={movieObj.id} className="card">
+            <img src={movieObj.imgURL} alt={movieObj.title} />
+            <h3>{movieObj.title}</h3>
+            <p>Rating: {movieObj.rating}</p>
+            <p>
+              <button onClick={() => deleteMovie(movieObj.id)}>Delete movie</button>
+            </p>
+          </div>
         );
       })}
     </>
